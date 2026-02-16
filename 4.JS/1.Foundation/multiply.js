2da16518fab844d2a6e843715cc5654a6c969f99
xDAnkit <xDAnkitjain@gmail.com>
2026-02-16 08:35:34 +0530

    Airthmetic Ops

---
+++ 4.JS/1.Foundation/multiply.js
@@ -0,0 +1,8 @@
+let num3 = 30;
+let num4 = 89;
+let mulResult = num3 * num4;
+console.log("Multilication result is : ", mulResult);
+
+// for (let i = 0; i < 100; i++) {
+//   console.log("i: ", i);
+// }
