0c10716b3832f042a589a2ed070b9f436f0c9cde
xDAnkit <xDAnkitjain@gmail.com>
2026-03-23 21:04:15 +0530

    Array options

---
+++ 5.Problem Solving/Day-4 Arrays/2.search-names-in-array.js
@@ -0,0 +1,29 @@
+const names = [
+  "Vidhya Skill School",
+  "Abhishek Garg",
+  "Almas Chamadiya",
+  "ANKIT RAJPUT",
+  "Ashitosh Vidhate",
+  "Aviral Jain",
+  "Ayush Gupta",
+  "Deepanshu Garg",
+  "Pinky Thawani",
+  "Prakash",
+  "Pranav Khare",
+  "Rakshit Dalal",
+  "Shivani Vishwakarma",
+  "Shruti Prajapati",
+  "vikas vanve",
+];
+
+const matchedName = (arr, char) => {
+  const results = arr.filter((item) => {
+    const currentWordLC = item.toUpperCase();
+    if (currentWordLC.search(char.toUpperCase()) !== -1) {
+      return true;
+    }
+    return false;
+  });
+
+  return results;
+};
